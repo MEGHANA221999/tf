ff96cbcaca5fb8d7ba62a46feacc2a80fb5581b6
meghana <meghana.mgowda1999@gmail.com>
2021-01-11 20:50:11 +0530

    add

---
+++ layer.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+access_key = "AKIAZSLA2ZCBPTRCYCNZ"
+secret_key = "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"
+region = "us-west-2"
+}
---
+++ module.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "ex" {
+ami = "ami-0a36eb8fadc976275"
+instance_type = "t2.micro"
+}
